440b4d0cde8b0dd8b1097d01390887d363eaad32
John Internet <jonjon1234.github@gmail.com>
2019-09-07 17:24:23 +0100

    Add comments.

--- examples/from_env.js
+++ examples/from_env.js
@@ -1,6 +1,7 @@
 // For production, replace ../env with @doctor_internet/interenv
 const EnvHandler = require("../env")
 
+// Create an example "environment".
 let envObj = {
 	NODE_ENV: "test",
 	TEST_VAR: "myVar",
@@ -8,10 +9,12 @@ let envObj = {
 	TEST_FLOAT: "${TEST_INT}.7"
 }
 
+// Load it into our handler and expand the vars.
 let env = new EnvHandler({
 	env: envObj,
 	expand: true
 })
 
+// Grab some data from it.
 console.log(env.isTesting())
 console.log(env.raw("TEST_FLOAT"))
