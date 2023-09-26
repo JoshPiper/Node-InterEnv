399e88afbed3937d2b2bdf3f9bcf57ea2b0224cb
JoshPiper <32164094+JoshPiper@users.noreply.github.com>
2023-09-26 01:58:11 +0100

    tests: Add test environment and interface.

--- test/environment.spec.ts
+++ test/environment.spec.ts
@@ -1,5 +1,23 @@
 import {assert} from "chai"
 import {Environment} from "../src/index"
+import {Environment, RaiseException, iEnvironment} from "../src/index"
+
+interface TestEnvironment extends iEnvironment {
+    NODE_ENV: string
+    TEST_INT: string
+    TEST_INT_HEX: string
+    TEST_FLOAT: string
+    TEST_BOOL_ON: string
+    TEST_BOOL_OFF: string
+}
+const test_environment: TestEnvironment = {
+    'NODE_ENV': 'test',
+    'TEST_INT': '1',
+    'TEST_INT_HEX': '0xff',
+    'TEST_FLOAT': '1.5',
+    'TEST_BOOL_ON': 'on',
+    'TEST_BOOL_OFF': '❌'
+}
 
 describe('Environment', () => {
     it('Can be created', () => {
